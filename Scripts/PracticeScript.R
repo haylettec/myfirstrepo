3eec1ea60537024213f646687160e16669c8c4df
haylettec <haylettec@appstate.edu>
2019-08-29 16:36:36 -0400

    Add new scripts and markdowns to folder

---
+++ Scripts/PracticeScript.R
@@ -0,0 +1,15 @@
+#this is a sample script
+
+#the only thing that can be in a Rscript are comments and code
+
+1 + 1
+1:10
+seq(from = 2, to = 100, by = 2)
+
+A <- 1 + 1
+B <- 1:10
+C <- seq(from = 2, to = 100, by = 2)
+
+3 * A
+
+5 * C
